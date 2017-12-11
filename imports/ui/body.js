fdf2bbc296fba899ad8a80375e77ef91282e4bac
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-12-11 16:27:15 +0200

    hide completed tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,7 +1,14 @@
+import { Template } from 'meteor/templating'
 //my template
-import './body.html'; 
+import './body.html';
 //other templates i want to use 
-import './list.js'; 
-import './add.js'; 
+import './list.js';
+import './add.js';
+
+Template.body.helpers({
+
+})
+  
+
 
 
--- imports/ui/list.js
+++ imports/ui/list.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 //database table
 import { Tasks } from '../api/tasks.js';
 
@@ -7,11 +8,29 @@ import './list.html';
 //other templates i want to use 
 import './task.js'; 
 
+
+Template.list.onCreated(function listOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.list.helpers({
-    tasks() {
-      //queue
-      // return Tasks.find({});
-      //stack ...
-      return Tasks.find({}, { sort: { createdAt: -1 } });
-    } 
-  });
+  tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    //queue
+    // return Tasks.find({});
+    // Otherwise, return all of the tasks
+    //stack ...
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+
+Template.list.events({
+  'change .hide-completed input'(event, instance){
+    instance.state.set('hideCompleted', event.target.checked);
+  },
+});
